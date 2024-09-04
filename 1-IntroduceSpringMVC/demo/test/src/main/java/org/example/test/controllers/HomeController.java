c660d20a6e1e75cf37b75a28aa0c95e4eaf80a16
MinhWaan <hoquan119@gmail.com>
2024-09-04 19:23:37 +0700

    Greeting Excercise and Demo

---
+++ 1-IntroduceSpringMVC/demo/test/src/main/java/org/example/test/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package org.example.test.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    @GetMapping("")
+    public String home() {
+        return "home";
+    }
+    @GetMapping("create")
+    public String create() {
+        return "create";
+    }
+}
---
+++ 1-IntroduceSpringMVC/demo/test/src/main/webapp/WEB-INF/views/create.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: LENOVO
+  Date: 9/4/2024
+  Time: 6:46 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Create</title>
+</head>
+<body>
+<h2>Create</h2>
+</body>
+</html>
